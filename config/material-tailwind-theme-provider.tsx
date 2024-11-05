a43548984870d7b760cc136eab2cb1938df72bc4
SongByeonghun <rokaf340@gmail.com>
2024-11-05 15:06:48 +0900

    remove: 중복파일 삭제

--- config/material-tailwind-theme-provider.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-
-export { ThemeProvider } from '@material-tailwind/react';
-
-// 서버 컴포넌트인 layout.tsx 파일에서 클라이언트에서 사용할 수 있는 ThemeProvider를 사용하면 에러발생
-// 이를 해결하기 위해 클라이언트 컴포넌트에서 ThemeProvider를 export해준다.
